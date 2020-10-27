f317ef93adcd479f0e7b5ae4be6a7e0c1e41f89d
Jakob Offersen <jakoboffersen@gmail.com>
2020-10-27 13:51:43 +0100

    changed aead encrypt/decrypt params back to using SecureBytes

--- Sodium/Aead+SecureBytes.swift
+++ Sodium/Aead+SecureBytes.swift
@@ -10,26 +10,27 @@ import Clibsodium
 
 
 extension Aead.ChaCha20Poly1305Ietf {
-    public func encrypt(message: Bytes, secretKey: SecureBytes, additionalData: SecureBytes? = nil, nonce: Nonce? = nil) -> (authenticatedCipherText: Bytes, nonce: Nonce)? {
+    public func encrypt(message: SecureBytes, secretKey: SecureBytes, additionalData: SecureBytes? = nil, nonce: Nonce? = nil) -> (authenticatedCipherText: SecureBytes, nonce: Nonce)? {
 
         guard secretKey.count == KeyBytes else { return nil }
 
-        var authenticatedCipherText = Bytes(count: message.count + ABytes)
+        guard let authenticatedCipherText = try? SecureBytes(count: message.count + ABytes) else { return nil }
         var authenticatedCipherTextLen: UInt64 = 0
 
 
         let nonce = nonce ?? self.nonce()
 
         guard .SUCCESS == crypto_aead_chacha20poly1305_ietf_encrypt(
-            &authenticatedCipherText, &authenticatedCipherTextLen,
-            message, UInt64(message.count),
+            authenticatedCipherText.pointer, &authenticatedCipherTextLen,
+            message.pointer, UInt64(message.count),
             additionalData?.pointer, UInt64(additionalData?.count ?? 0),
-                nil, nonce, secretKey.pointer).exitCode else { return nil }
+            nil, nonce, secretKey.pointer).exitCode
+        else { return nil }
 
         return (authenticatedCipherText: authenticatedCipherText, nonce: nonce)
     }
 
-    public func decrypt(authenticatedCipherText: Bytes, secretKey: SecureBytes, nonce: Nonce, additionalData: SecureBytes? = nil) -> SecureBytes? {
+    public func decrypt(authenticatedCipherText: SecureBytes, secretKey: SecureBytes, nonce: Nonce, additionalData: SecureBytes? = nil) -> SecureBytes? {
         guard authenticatedCipherText.count >= ABytes else { return nil }
 
         guard let message = try? SecureBytes(count: authenticatedCipherText.count - ABytes) else { return nil }
@@ -38,9 +39,10 @@ extension Aead.ChaCha20Poly1305Ietf {
         guard .SUCCESS == crypto_aead_chacha20poly1305_ietf_decrypt(
             message.pointer, &messageLen,
             nil,
-            authenticatedCipherText, UInt64(authenticatedCipherText.count),
+                authenticatedCipherText.pointer, UInt64(authenticatedCipherText.count),
             additionalData?.pointer, UInt64(additionalData?.count ?? 0),
-            nonce, secretKey.pointer).exitCode else { return nil }
+            nonce, secretKey.pointer).exitCode
+        else { return nil }
 
         return message
     }
--- Tests/SodiumTests/SodiumTests.swift
+++ Tests/SodiumTests/SodiumTests.swift
@@ -496,7 +496,7 @@ class SodiumTests: XCTestCase {
     }
 
     func testChaCha20Poly1305IetfSecureBytes() {
-        let message = "I am message".bytes
+        let message = try! SecureBytes(bytes: "I am message".bytes)
         let additionalData = try! SecureBytes(bytes: "I am additionalData".bytes)
 
         let secretKey = sodium.aead.chacha20poly1305ietf.secureBytesKey()!
@@ -505,8 +505,7 @@ class SodiumTests: XCTestCase {
         let (authenticatedCipherTextWithAdditionalData, nonceWithAdditionlData) = sodium.aead.chacha20poly1305ietf.encrypt(message: message, secretKey: secretKey, additionalData: additionalData)!
         let decrypted2 = sodium.aead.chacha20poly1305ietf.decrypt(authenticatedCipherText: authenticatedCipherTextWithAdditionalData, secretKey: secretKey, nonce: nonceWithAdditionlData, additionalData: additionalData)!
 
-        let expected = try! SecureBytes(bytes: message)
-        XCTAssertTrue(decrypted2 == expected)
+        XCTAssertTrue(decrypted2 == message)
     }
 
     func testSecureBytesConcat() {
